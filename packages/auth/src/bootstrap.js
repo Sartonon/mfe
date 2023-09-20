6ed10d75a4cd8be94b35d955bf69621f866a550b
Santeri Rusila <santtu_1993@hotmail.com>
2023-09-20 22:14:24 +0300

    finalize

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,23 +4,26 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
-      initialEntries: [document.location.pathname],
+      initialEntries: [initialPath],
     });
 
-  history.listen(onNavigate);
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
-      if (history.location.pathname !== nextPathname) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-      console.log("The container just navigated");
     },
   };
 };
@@ -28,7 +31,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
